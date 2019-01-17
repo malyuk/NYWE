1c50e832615c970da5857e2a6612deca1778fbf4
Calvin Koepke <hello@calvinkoepke.com>
2019-01-16 17:15:23 -0700

    WIP: Begin seed function

--- mu-plugins/app/src/seeds/convert-fields.php
+++ mu-plugins/app/src/seeds/convert-fields.php
@@ -79,14 +79,133 @@ add_seed( [
 
 		}
 
-		add_admin_notice( 'Metadata updated' );
+		echo 'Metadata updated';
 
 	}
 ] );
 
+add_seed([
+	'name'        => 'Migrate Event Metadata to ACF',
+	'description' => 'Migrates all meta box data on the Event custom post type to the correct ACF fields.',
+	'callback'    => function() {
+
+		$events = get_posts([
+			'post_type'   => 'events',
+			'post_status' => 'publish'
+		]);
+
+		$include = get_old_event_meta_keys();
+
+		foreach( $events as $event ) {
+
+			// Get all metadata.
+			$data = get_metadata( 'post', $event->ID, '' );
+
+			// Filter the metadata to only contain the old keys.
+			$data = array_intersect_key( $data, array_combine( $include, $include ) );
+
+			foreach( $data as $meta ) {
+
+				$val = isset( $meta[0] ) ? $meta[0] : null;
+
+				if ( is_null( $val ) ) {
+					continue;
+				}
+
+				$id = get_image_id( $val );
+
+			}
+		}
+	}
+]);
+
 function get_image_id( $image_url ) {
+
 	global $wpdb;
+
+	if ( empty( $image_url ) ) {
+		return false;
+	}
+
 	$attachment = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url ) );
 
-	return $attachment[0];
+	return (isset( $attachment[0] ) && ! empty( $attachment[0] ) ) ? $attachment[0] : false;
+
+}
+
+function get_old_event_meta_keys() {
+	return [
+		'eventSoldout',
+		'events_list_cell_image',
+		'events_list_cell_image_logo',
+		'eventDate',
+		'eventTime',
+		'eventCity',
+		'eventPurchaseLink',
+		'eventPresentingSponsor',
+		'eventPresentingSponsorUrl',
+		'eventIntroImage',
+		'eventIntroHeading',
+		'eventIntroText',
+		'eventIntroItem1_heading',
+		'eventIntroItem1_text',
+		'eventIntroItem2_heading',
+		'eventIntroItem2_text',
+		'eventFeedback_heading_1',
+		'eventFeedback_client_1',
+		'eventFeedback_date_1',
+		'eventFeedback_excerpt_1',
+		'eventFeedback_heading_2',
+		'eventFeedback_client_2',
+		'eventFeedback_date_2',
+		'eventFeedback_excerpt_2',
+		'eventFeedback_heading_3',
+		'eventFeedback_client_3',
+		'eventFeedback_date_3',
+		'eventFeedback_excerpt_3',
+		'eventVideo',
+		'eventIntro2Image',
+		'eventIntro2Heading',
+		'eventContinuedIntroText',
+		'eventIntro3Image',
+		'eventIntro3Heading',
+		'eventContinuedIntroText3',
+		'eventIntro4Image',
+		'eventIntro4Heading',
+		'eventContinuedIntroText4',
+		'eventIntro5Image',
+		'eventIntro5Heading',
+		'eventContinuedIntroText5',
+		'eventMap',
+		'eventMapUrl',
+		'eventTicketsCard_1_subHeading',
+		'eventTicketsCard_1_heading',
+		'eventTicketsCard_1_text',
+		'eventTicketsCard_1_price',
+		'eventTicketsCard_1_purchase',
+		'eventTicketsCard_1_also_1',
+		'eventTicketsCard_1_also_2',
+		'eventTicketsCard_1_also_3',
+		'eventTicketsCard_1_also_4',
+		'eventTicketsCard_2_subHeading',
+		'eventTicketsCard_2_heading',
+		'eventTicketsCard_2_text',
+		'eventTicketsCard_2_price',
+		'eventTicketsCard_2_purchase',
+		'eventTicketsCard_2_also_1',
+		'eventTicketsCard_2_also_2',
+		'eventTicketsCard_2_also_3',
+		'eventTicketsCard_2_also_4',
+		'eventTicketsCard_3_subHeading',
+		'eventTicketsCard_3_heading',
+		'eventTicketsCard_3_text',
+		'eventTicketsCard_3_price',
+		'eventTicketsCard_3_purchase',
+		'eventTicketsCard_3_also_1',
+		'eventTicketsCard_3_also_2',
+		'eventTicketsCard_3_also_3',
+		'eventTicketsCard_3_also_4',
+		'advert',
+		'advertHead',
+	];
 }
