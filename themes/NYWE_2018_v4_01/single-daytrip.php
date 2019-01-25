8f9b5bcc15416559c4b627d729ee749884a64a2b
Calvin Koepke <hello@calvinkoepke.com>
2019-01-25 14:00:45 -0700

    update cta

--- themes/NYWE_2018_v4_01/single-daytrip.php
+++ themes/NYWE_2018_v4_01/single-daytrip.php
@@ -64,7 +64,7 @@
 							<div class="response" id="mce-success-response" style="display:none"></div>
 						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_da778b0b532a20f59d31779a3_462f8355e6" tabindex="-1" value=""></div>
-						<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						<div class="clear"><input type="submit" value="GET AN EXCLUSIVE DISCOUNT EMAILED TO YOU NOW!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 					</form>
 				</div>
 				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='zip';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
