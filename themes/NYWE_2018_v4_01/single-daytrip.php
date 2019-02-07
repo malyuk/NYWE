b931a9929cd0750d3b25d4dbe90c231469261cff
Calvin Koepke <hello@calvinkoepke.com>
2019-02-07 10:06:13 -0700

    remove js validation

--- themes/NYWE_2018_v4_01/single-daytrip.php
+++ themes/NYWE_2018_v4_01/single-daytrip.php
@@ -67,7 +67,7 @@
 						<div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 					</form>
 				</div>
-				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='zip';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			
 			<?php endif; ?>
 
 			<!-- INTRODUCTION MODULE -->
