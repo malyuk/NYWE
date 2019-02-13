183daad0e02a510088ca8ad43fd65c14b3520d73
Calvin Koepke <hello@calvinkoepke.com>
2019-02-13 10:45:02 -0700

    fix

--- themes/NYWE_2018_v4_01/single-events.php
+++ themes/NYWE_2018_v4_01/single-events.php
@@ -522,9 +522,13 @@ if ( ! empty( $advertHead ) ) {
 								<?php
 								$event_date = get_field( 'event_date' );
 								if ( ! empty( $event_date ) ) {
+
+									$event_date = date( 'F d, Y', strtotime( $event_date ) );
+
 									printf( '<h3 class="related_list_cell_image_overlay_date">%s</h3>',
 										esc_html( $event_date )
 									);
+									
 								} ?>
 							</div><!-- overlay -->
 							<div class="related_list_cell_card">
