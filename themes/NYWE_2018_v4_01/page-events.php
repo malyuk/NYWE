c4bfc9cb897655e3ae0a366f53d17aeddfc126be
Calvin Koepke <hello@calvinkoepke.com>
2019-02-13 12:22:52 -0700

    update purchase links

--- themes/NYWE_2018_v4_01/page-events.php
+++ themes/NYWE_2018_v4_01/page-events.php
@@ -2,6 +2,8 @@
 /*
 Template Name: Events v4
 */
+
+$event_purchase_link = get_field( 'event_purchase_link', get_the_ID() );
 ?>
 
 <?php get_header(); ?>
@@ -171,7 +173,7 @@ Template Name: Events v4
 	                    if ( $event_sold_out ) {
 		                    echo '<a class="events_list_cell_card_purchase sold_out" href="' . get_the_permalink() . '">SOLD OUT</a>';
 	                    } else {
-		                    echo '<a class="events_list_cell_card_purchase" href="' . get_the_permalink() . '">PURCHASE TICKETS</a>';
+		                    echo '<a class="events_list_cell_card_purchase" href="' . esc_url( $event_purchase_link ) . '">PURCHASE TICKETS</a>';
 	                    } ?>
                     </div><!-- card -->
                 </div><!-- content -->    
